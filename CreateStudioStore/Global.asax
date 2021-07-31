9419ade8dd7a74b6550540aefee9c4b92e2655d5
Taheny <taheny.belguith@gmail.com>
2021-07-31 10:45:35 +0100

    Add project files.

---
+++ CreateStudioStore/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CreateStudioStore.MvcApplication" Language="C#" %>
---
+++ CreateStudioStore/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CreateStudioStore.Startup))]
+namespace CreateStudioStore
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
